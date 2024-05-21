ebc425deca2663b92ce50fe7b0f0cd32de037cb1
bsquiroz <bemq1605@gmail.com>
2024-05-20 21:40:10 -0500

    add styles percent

--- src/components/InfoBudge/components/InfoBudgeMain/InfoBudgeMain.vue
+++ src/components/InfoBudge/components/InfoBudgeMain/InfoBudgeMain.vue
@@ -1,25 +1,41 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { useStore } from "../../../../composable/useStore";
 import { formatPrice } from "../../../../utils/formatPrice";
 
 const { valuesBudge, handleShowModalBudge } = useStore();
+
+const colorPercent = computed(() => {
+  if (valuesBudge.percent > 70) return "#990000";
+  if (valuesBudge.percent > 40) return "#F9D923";
+  return "#367E18";
+});
 </script>
 
 <template>
-  <section class="grid grid-cols-3">
-    <div class="col-span-1 grid place-content-center">
-      <p class="text-xl text-white font-bold">
-        {{ Number(valuesBudge.percent).toFixed(2) }}%
-      </p>
-
-      <div :style="`--percent: ${valuesBudge.percent}`">
+  <section class="grid grid-cols-3 gap-4">
+    <div class="col-span-3 md:col-span-1 grid place-content-center">
+      <div class="relative" :style="`--percent: ${valuesBudge.percent}`">
+        <p
+          class="text-xl text-white font-bold absolute inset-0 w-fit h-fit m-auto"
+          :style="{ color: colorPercent }"
+        >
+          {{ Number(valuesBudge.percent).toFixed(2) }}%
+        </p>
         <svg>
           <circle class="circle" r="65" cx="50%" cy="50%" pathLength="100" />
-          <circle class="circle" r="65" cx="50%" cy="50%" pathLength="100" />
+          <circle
+            class="circle"
+            r="65"
+            cx="50%"
+            cy="50%"
+            pathLength="100"
+            :style="{ stroke: colorPercent }"
+          />
         </svg>
       </div>
     </div>
-    <div class="col-span-2 flex flex-col gap-2">
+    <div class="col-span-3 md:col-span-2 flex flex-col gap-2">
       <p class="text-xl text-green-500">
         <span class="font-bold">Presupuesto: </span>
         {{ formatPrice(valuesBudge.bugbe) }}
@@ -55,7 +71,6 @@ const { valuesBudge, handleShowModalBudge } = useStore();
 }
 
 .circle:nth-child(2) {
-  stroke: lightcoral;
-  stroke-dasharray: var(--percent) 100;
+  stroke-dasharray: calc(var(--percent) + 1) 100;
 }
 </style>
